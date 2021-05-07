48ca51b4e66eecfce237e7fb1f96eb447f5ee427
Daman Morris <damanm72@gmail.com>
2021-05-07 10:59:42 -0400

    Add service and implementation for ReportingStructure

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee read(String id) {
+        LOG.debug("Creating employee with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        LOG.debug("Creating reporting structure for employee");
+
+        ReportingStructure reportingStructure = new ReportingStructure();
+        reportingStructure.setEmployee(employee);
+
+        return reportingStructure.getNumberOfReports();
+    }
+}
